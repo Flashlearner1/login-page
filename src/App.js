4b9e142c290742366f8cf1d90e8d0feddb0069d3
Flashlearner1 <osayandekingsley768@gmail.com>
2023-08-10 06:48:49 -0700

    loginpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" 
+          placeholder="Enter your Email address" 
+          id="Email"
+          onChange={handleUsernameChange}/>
+        </div>
+        <div>
+          <input type="password" 
+          placeholder="Enter your Password" 
+          id="Password"
+          onChange={handlePasswordChange}/>
+        </div>
+      </form>
+
+<br />
+      <div>
+        <div><button type='submit'>Login</button></div>
+        <a href="#">forgotten password?</a>
+        <div className= "btn"><button type='New'>Create New Account</button></div>
+      </div>
+
     </div>
   );
 }
